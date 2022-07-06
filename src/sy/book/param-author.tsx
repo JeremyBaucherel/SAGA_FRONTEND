4ac7f3824782a2735a5182b43025d37dc97e8361
JeremyBaucherel <61932395+JeremyBaucherel@users.noreply.github.com>
2022-07-06 09:58:00 +0200

    Ajout du job

--- src/sy/book/param-author.tsx
+++ src/sy/book/param-author.tsx
@@ -16,6 +16,8 @@ interface ParamProps {
 interface ParamState {
 	resultAdd: any;
 	requestStatusAdd: Common.ECallStatus;
+	resultListeJob: any;
+	requestStatusListeJob: Common.ECallStatus;
 	addRow: number;
 }
 
@@ -28,9 +30,36 @@ export class ParamComp extends React.PureComponent<ParamProps,ParamState> {
 			resultAdd: {},
 			requestStatusAdd: Common.ECallStatus.NOT_STARTED,
 			addRow:0,
+			resultListeJob: {},
+			requestStatusListeJob: Common.ECallStatus.NOT_STARTED,
 		}
 
 		this.handleAddCell = this.handleAddCell.bind(this);
+	}
+
+	componentDidMount (): void {
+		this.requestListeJob();
+	}
+
+    requestListeJob (): void {
+		this.setState({requestStatusListeJob: Common.ECallStatus.RUNNING});
+        let url_ = new Common.Url(['api', 'bibliotheque', 'liste', 'job']);
+        Common.postAsJson(url_, {authorization:"BOOK:DISPLAY"}, this.receiveDataListeJob.bind(this), this.receiveDataListeJobError.bind(this));
+	}
+
+	receiveDataListeJob (resp: Common.IResponse<any>): void {
+		let myresult = resp.body;
+		this.setState({
+			requestStatusListeJob: Common.ECallStatus.OK,
+			resultListeJob: myresult,
+		});
+	}
+
+	receiveDataListeJobError (): void {
+		this.setState({
+			requestStatusListeJob: Common.ECallStatus.NOK,
+			resultListeJob:[]
+		});
 	}
 
 	handleAddCell(addRow:any){
@@ -59,8 +88,10 @@ export class ParamComp extends React.PureComponent<ParamProps,ParamState> {
 	}
 
 	render (): React.ReactNode {
+
 		let col = [
 			new SpreadsheetColumn('name_author', 'Nom de l\'auteur', 500, "text", "text", true, true, true, true, true),
+			new SpreadsheetColumn('name_job', 'Nom du métier', 500, "menu", "liste", true, true, true, true, true, this.state.resultListeJob, true),
 		];
 
 		let title="Bibliothèque - Paramétrage des Auteurs";
@@ -91,7 +122,6 @@ export class ParamComp extends React.PureComponent<ParamProps,ParamState> {
 						style={{textAlign:"center"}}
 						add={add}
 						onAddCell={this.handleAddCell}
-
 						/>);
 					} else {
 						pageBody = (<NotAuthorizedAlert />);
